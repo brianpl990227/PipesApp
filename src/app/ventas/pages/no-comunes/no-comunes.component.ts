ad23ef1e8e151b8489ef5cf020dad530588b5ced
Brian Pérez López <brianpl990227@gmail.com>
2023-03-24 00:56:56 -0400

    Seccion pipes de angular completada

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -14,15 +15,42 @@ export class NoComunesComponent {
     'femenino': 'invitarla'
   }
 
+  persona = {
+    nombre: 'Brian',
+    edad: 24,
+    direccion: 'La Habana, Cuba (por desgracia)'
+  }
+
+  heroes = [
+    {
+      nombre: 'Ironman',
+      edad: 33,
+      poder: 'Vuela'
+    },
+    {
+      nombre: 'Spiderman',
+      edad: 17,
+      poder: 'Araña' 
+    }
+  ]
+
+  miObservable = interval(1000)
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Fin de la promesa')
+    }, 5000)
+  })
+
   //i18nPlural
-  clientes: string[] = ['Maria', 'Perdro', '', '', '', '']
+  clientes: string[] = ['Maria', 'Perdro', 'Brian', 'Adrian', 'Mengao', 'Usuario']
   clientesMap = {
     '=0': 'no tenemos ningún cliente esperando.',
     '=1': 'tenemos un cliente esperando.',
     'other': 'tenemos # clientes esperando'
 
 
-    
+
   }
 
   cambiarNombre()
